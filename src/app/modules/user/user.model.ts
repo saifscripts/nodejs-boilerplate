c04c4de80185409f1619c22b4e9440178ab4d383
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-19 05:15:14 +0600

    add comments on model

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -26,10 +26,12 @@ const userSchema = new Schema<IUser, UserModel, IUserMethods>(
   },
 );
 
+// virtuals: derive a virtual field from the db fields
 userSchema.virtual('fullName').get(function () {
   return 'Mr. ' + this.name;
 });
 
+// DOCUMENT MIDDLEWARE - pre hook(save): runs before save/create
 userSchema.pre('save', async function (next) {
   const user: IUser = this;
   user.password = await bcrypt.hash(
@@ -39,26 +41,31 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// DOCUMENT MIDDLEWARE - post hook(save): runs after save/create
 userSchema.post('save', function (doc, next) {
   doc.password = '';
   next();
 });
 
+// QUERY MIDDLEWARE - pre hook(find): runs before find query
 userSchema.pre('find', function (next) {
   this.find({ isDeleted: { $ne: true } });
   next();
 });
 
+// QUERY MIDDLEWARE - pre hook(findOne): runs before findOne query
 userSchema.pre('findOne', function (next) {
   this.find({ isDeleted: { $ne: true } });
   next();
 });
 
+// Mongoose methods (instance methods)
 userSchema.methods.isEmailExists = async function (email) {
   const result = await User.findOne({ email });
   return result;
 };
 
+// Mongoose statics (static methods)
 userSchema.statics.isMobileExists = async function (mobile) {
   const result = await this.findOne({ mobile });
   return result;
